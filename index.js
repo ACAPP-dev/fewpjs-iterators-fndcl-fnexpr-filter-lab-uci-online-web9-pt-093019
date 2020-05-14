aa1676ffb38513e90bb723a4b4a7af3ede566694
ACAPP-dev <acapp909@gmail.com>
2020-05-14 16:27:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(drivers, filter) {
+    return drivers.filter(driver => {
+        return driver.toLowerCase() === filter.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, testFilter) {
+    return drivers.filter(driver => {
+        return driver.match(`^${testFilter}`)
+    })
+}
+
+function matchName(drivers, filter) {
+    return drivers.filter(driver => {
+        return driver.name === filter
+    })
+}
